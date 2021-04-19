1eada9f562a46a58d86566ac580f5b3aba0f4c08
w9_th <william.tan@onethesis.com>
2021-04-20 00:21:31 +0800

    Add project files.

---
+++ WindowsFormsApp_FFMPEG_Batch/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace WindowsFormsApp_FFMPEG_Batch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select folder where .mjpeg files reside:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(509, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 190);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(509, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select folder where ffmpeg.exe reside:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(576, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(370, 72);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Convert with FFMPEG";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(202, 377);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(368, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WindowsFormsApp_FFMPEG_Batch/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace WindowsFormsApp_FFMPEG_Batch
+{
+    public partial class Form1 : Form
+    {
+        private List<string> mjpegFiles = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                textBox1.Text = folderBrowserDialog.SelectedPath;
+                button2.Enabled = true;
+                string[] files = Directory.GetFiles(folderBrowserDialog.SelectedPath);
+                mjpegFiles.Clear();
+                foreach(string s in files)
+                {
+                    mjpegFiles.Add(s);
+                }
+                System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                textBox2.Text = folderBrowserDialog.SelectedPath;
+                MessageBox.Show(folderBrowserDialog.SelectedPath);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //ProcessStartInfo startInfo = new ProcessStartInfo();
+            //startInfo.WorkingDirectory = @"C:\mjpeg mp4 test files";
+            //startInfo.CreateNoWindow = false;
+            //startInfo.UseShellExecute = false;
+            //startInfo.FileName = @"C:\ffmpeg-4.4-full_build\bin\ffmpeg.exe";
+            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            //startInfo.Arguments = @"-i 20210208074105.mjpeg output.mp4";
+
+            //startInfo.RedirectStandardOutput = true;
+            //startInfo.RedirectStandardError = true;
+
+            //using(Process exeProcess = Process.Start(startInfo))
+            //{
+            //    string error = exeProcess.StandardError.ReadToEnd();
+            //    string output = exeProcess.StandardError.ReadToEnd();
+            //    exeProcess.WaitForExit();
+
+            //    MessageBox.Show("Error:" + error);
+            //    MessageBox.Show("Output:" + output);
+            //}
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WorkingDirectory = textBox1.Text,
+                //WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                FileName = textBox2.Text + @"\ffmpeg.exe"
+            };
+            string cmdToSend = @"-i 20210208074105.mjpeg output.mp4";
+            startInfo.Arguments = cmdToSend;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
