1b5f1ead5236b98921b9781837ebe6d2038bdc13
w9_th <william.tan@onethesis.com>
2021-04-20 00:26:01 +0800

    ffmpeg conversion of mjpeg to mp4 file works next is to work on batch conversion of all mjpeg files available

--- WindowsFormsApp_FFMPEG_Batch/Form1.cs
+++ WindowsFormsApp_FFMPEG_Batch/Form1.cs
@@ -52,27 +52,6 @@ namespace WindowsFormsApp_FFMPEG_Batch
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //ProcessStartInfo startInfo = new ProcessStartInfo();
-            //startInfo.WorkingDirectory = @"C:\mjpeg mp4 test files";
-            //startInfo.CreateNoWindow = false;
-            //startInfo.UseShellExecute = false;
-            //startInfo.FileName = @"C:\ffmpeg-4.4-full_build\bin\ffmpeg.exe";
-            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            //startInfo.Arguments = @"-i 20210208074105.mjpeg output.mp4";
-
-            //startInfo.RedirectStandardOutput = true;
-            //startInfo.RedirectStandardError = true;
-
-            //using(Process exeProcess = Process.Start(startInfo))
-            //{
-            //    string error = exeProcess.StandardError.ReadToEnd();
-            //    string output = exeProcess.StandardError.ReadToEnd();
-            //    exeProcess.WaitForExit();
-
-            //    MessageBox.Show("Error:" + error);
-            //    MessageBox.Show("Output:" + output);
-            //}
-
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
             {
